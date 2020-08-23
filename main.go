4a8094b4af58c6c057c6e6720414824d498cee0c
Cuong Tran <103cuong@gmail.com>
2020-08-23 16:03:19 +0700

    feat: add .gitattributes

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber"
 )
 
 func main() {
-	fmt.Println("💅")
+	// Define constants
+	const port = 3000
+
+	// Fiber instance
+	app := fiber.New()
+
+	// Routes
+	app.Get("/", hello)
+
+	// 404 Handler
+	app.Use(func(c *fiber.Ctx) {
+		// nolint: gomnd
+		c.SendStatus(404)
+	})
+
+	// Start server
+	log.Fatal(app.Listen(port))
+}
+
+// Handler
+func hello(c *fiber.Ctx) {
+	c.Send("Hello, World 👋!")
 }
